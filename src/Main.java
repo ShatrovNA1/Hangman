67506b704e0087a1e509a2a3348b1e68d80a8760
ShatrovNA <shatrovna1@gmail.com>
2026-09-04 21:31:33 +0500

    Реализация игры "Виселица"

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,221 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-void main() {
-    //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-    // to see how IntelliJ IDEA suggests fixing it.
-    IO.println(String.format("Hello and welcome!"));
+import java.util.Collections;
 
-    for (int i = 1; i <= 5; i++) {
-        //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-        // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-        IO.println("i = " + i);
+Random random = new Random();
+
+enum State {
+    MENU,
+    GAME,
+    EXIT,
+}
+
+List<String> getAllWords() {
+    try (FileReader fileReader = new FileReader("dictionary.txt")) {
+        return fileReader.readAllLines();
+    } catch (Exception _) {
+        System.out.println("Проблема с загрузкой словаря из файла");
+        return Collections.emptyList();
     }
+}
+
+void printMenu() {
+    IO.println();
+    IO.println("====== ВИСЕЛИЦА ======");
+    IO.println("1) Новая игра");
+    IO.println("2) Выход");
+    IO.println("======================");
+    IO.println("Для выбора введите число:");
+}
+
+void printWord(String word, Set<Character> guessedLetters) {
+    for (char c : word.toCharArray()) {
+        if (guessedLetters.contains(c)) {
+            IO.print(c + " ");
+        } else {
+            IO.print("_ ");
+        }
+    }
+    IO.println();
+}
+
+String getRandomWord(List<String> allWords) {
+    int index = random.nextInt(allWords.size());
+    return allWords.get(index);
+}
+
+void printHangman(int attempts) {
+    String[] hangman = {
+            """
+             +---+
+             |   |
+                 |
+                 |
+                 |
+                 |
+            =========
+            """,
+
+            """
+             +---+
+             |   |
+             O   |
+                 |
+                 |
+                 |
+            =========
+            """,
+
+            """
+             +---+
+             |   |
+             O   |
+             |   |
+                 |
+                 |
+            =========
+            """,
+
+            """
+             +---+
+             |   |
+             O   |
+            /|   |
+                 |
+                 |
+            =========
+            """,
+
+            """
+             +---+
+             |   |
+             O   |
+            /|\\  |
+                 |
+                 |
+            =========
+            """,
+
+            """
+             +---+
+             |   |
+             O   |
+            /|\\  |
+            /    |
+                 |
+            =========
+            """,
+
+            """
+             +---+
+             |   |
+             O   |
+            /|\\  |
+            / \\  |
+                 |
+            =========
+            """
+    };
+
+    IO.println(hangman[Math.min(attempts, 6)]);
+}
+
+boolean isWordGuessed(String word, Set<Character> guessedLetters) {
+    for (char c : word.toCharArray()) {
+        if (!guessedLetters.contains(c)) {
+            return false;
+        }
+    }
+
+    return true;
+}
+
+void startGame(List<String> allWords) {
+    String word = getRandomWord(allWords);
+    Set<Character> guessedLetters = new HashSet<>();
+
+    int attempts = 0;
+    final int MAX_ATTEMPTS = 6;
+
+    while (attempts < MAX_ATTEMPTS) {
+
+        IO.println();
+        IO.println("Количество ошибок: " + attempts + " из " + MAX_ATTEMPTS);
+
+        printHangman(attempts);
+
+        IO.print("Слово: ");
+        printWord(word, guessedLetters);
+
+        IO.print("Введите букву: ");
+        String input = IO.readln().trim().toLowerCase();
+        if (input.length() != 1 || !Character.isLetter(input.charAt(0))) {
+            IO.println("Введите одну букву.");
+            continue;
+        }
+
+        char letter = input.charAt(0);
+
+        if (letter < 'а' || letter > 'я') {
+            IO.println("Введите букву русского алфавита не заглавную");
+            continue;
+        }
+
+        if (guessedLetters.contains(letter)) {
+            IO.println("Вы уже вводили эту букву.");
+            continue;
+        }
+
+        guessedLetters.add(letter);
+        if (word.indexOf(letter) >= 0) {
+            IO.println("Правильно!");
+        } else {
+            attempts++;
+            IO.println("Такой буквы нет.");
+        }
+
+        if (isWordGuessed(word, guessedLetters)) {
+            IO.println();
+            IO.println("ПОБЕДА!");
+            IO.println("Слово: " + word);
+            return;
+        }
+    }
+
+    printHangman(MAX_ATTEMPTS);
+
+    IO.println();
+    IO.println("Поражение!");
+    IO.println("Загаданное слово: " + word);
+}
+
+void main() {
+
+    List<String> allWords = getAllWords();
+
+    if (allWords.isEmpty()) {
+        return;
+    }
+
+    State gameState = State.MENU;
+
+    while (gameState != State.EXIT) {
+        if (gameState == State.MENU) {
+            printMenu();
+            String input = IO.readln();
+            switch (input) {
+                case "1":
+                    gameState = State.GAME;
+                    break;
+                case "2":
+                    gameState = State.EXIT;
+                    break;
+                default:
+                    IO.println("Неправильный ввод.");
+            }
+        } else if (gameState == State.GAME) {
+            startGame(allWords);
+            gameState = State.MENU;
+        }
+    }
+
+    IO.println("До свидания!");
 }
